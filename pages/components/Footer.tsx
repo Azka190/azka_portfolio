1a01484ef8d8db33047f21a87a809cf6757fd6f5
Azka190 <azkarehman2001@gmail.com>
2023-04-01 11:56:06 +0500

    Create Footer.tsx

---
+++ pages/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer className="bg-gray-800 py-4">
+  <div className="container mx-auto flex justify-center items-center">
+    <p className="text-gray-400">&copy; 2023 Azka Rehman</p>
+   
+  </div>
+</footer>
+
+    </div>
+  )
+}
+
+export default Footer
